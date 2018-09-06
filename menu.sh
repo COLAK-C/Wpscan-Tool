5510d2448d423656bc199704b17eaa78a231dcce
ÇOLAK <39554929+COLAK-C@users.noreply.github.com>
2018-09-06 15:30:07 +0300

    menu.sh

--- menu.sh
+++ menu.sh
@@ -358,7 +358,7 @@ echo -e "${WHITE}
 ╚███╔███╔╝██║     ███████║╚██████╗██║  ██║██║ ╚████║       ██║   ╚██████╔╝╚██████╔╝███████╗${BLUE}
  ╚══╝╚══╝ ╚═╝     ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝${WHITE}
 "${WHITE}                                                                                           
-    echo -e "${WHITE}CODED BY YANLIZ KURT ${GREEN}DARKSECARMY.COM ${RED}[NO EDIT] "
+    echo -e "${WHITE}CODED BY COLAK ${GREEN}DARKSECARMY.COM ${RED}[NO EDIT] "
     echo -e "${WHITE} "${GREEN}CTRL + Z YAPARAK SCRIPTDEN CIKIS YAPABILIRSINIZ
     echo -e "${RED}==================== PARAMETRELER ====================+ "	
     echo -e "${LIGHTBLUE}[1]${WHITE}PLUGIN LISTELEMEK${LIGHTBLUE}                                  | " 
